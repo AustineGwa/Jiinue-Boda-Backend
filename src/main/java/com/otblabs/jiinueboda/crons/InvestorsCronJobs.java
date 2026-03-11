882f0f1aae4f07c09edea912ae4b6f4addb0f14e
AustineGwa <austinegwa64@gmail.com>
2026-03-12 00:28:48 +0300

    set investor earnings cron job

--- src/main/java/com/otblabs/jiinueboda/crons/InvestorsCronJobs.java
+++ src/main/java/com/otblabs/jiinueboda/crons/InvestorsCronJobs.java
@@ -1,6 +1,7 @@
 package com.otblabs.jiinueboda.crons;
 
 import com.otblabs.jiinueboda.investors.InvestmentManagementService;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,8 +13,14 @@ public class InvestorsCronJobs {
         this.investmentManagementService = investmentManagementService;
     }
 
+    @Scheduled(cron = "0 0 0 11 * ?")
     void sendSogomoMonthlyInterest(){
         investmentManagementService.sendPeriodicInterestToInvestor("542542","03405413296150");
+    }
+
+    @Scheduled(cron = "0 0 0 15 * ?")
+    void sendClifMonthlyInterest(){
+        investmentManagementService.sendPeriodicInterestToInvestor("522522","1298739764");
     }
 
 
