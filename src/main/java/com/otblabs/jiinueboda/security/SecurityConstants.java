f15f30ce3e99f9bc0eca1f0d7be8db6e1b61cc34
AustineGwa <austinegwa64@gmail.com>
2026-03-18 06:21:21 +0300

    rollback security secret

--- src/main/java/com/otblabs/jiinueboda/security/SecurityConstants.java
+++ src/main/java/com/otblabs/jiinueboda/security/SecurityConstants.java
@@ -1,18 +1,8 @@
 package com.otblabs.jiinueboda.security;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-@Component
 public class SecurityConstants {
-
-    public static String SECRET;
-
-    @Value("${fintech.SECRET_KEY}")
-    public void setSecret(String secret) {
-        SECRET = secret;
-    }
-
+    static int latestAppVersion = 2;
+    public static final String SECRET = "ef18adcd-8eac-4aa4-bd9b-9fbebde4b78c-driversCC" + latestAppVersion;
     public static final long EXPIRATION_TIME = 43_200_000;
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
