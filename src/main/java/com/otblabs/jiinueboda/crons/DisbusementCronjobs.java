aadabde82d3f18b6ddf9787a1f41ad23a5148d05
AustineGwa <austinegwa64@gmail.com>
2026-03-19 10:56:25 +0300

    bug fix

--- src/main/java/com/otblabs/jiinueboda/crons/DisbusementCronjobs.java
+++ src/main/java/com/otblabs/jiinueboda/crons/DisbusementCronjobs.java
@@ -1,9 +1,13 @@
 package com.otblabs.jiinueboda.crons;
 
+import com.otblabs.jiinueboda.integrations.momo.mpesa.b2c.models.B2CRequestResponse;
 import com.otblabs.jiinueboda.investors.InvestmentManagementService;
 import com.otblabs.jiinueboda.loans.LendingService;
+import com.otblabs.jiinueboda.loans.models.Loan;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class DisbusementCronjobs {
@@ -17,29 +21,29 @@ public class DisbusementCronjobs {
         this.investmentManagementService = investmentManagementService;
     }
 
-//    @Scheduled(fixedRate = 1000) //every second
-//    public void disburseLoans(){
-//        List<Loan> pendingDisbursments = lendingService.getAllPendingDisbursements();
-//        pendingDisbursments.forEach(loan->{
-//
-//            try {
-//
-//                if(investmentManagementService.investorDisburse(loan)){
-//                    lendingService.setLoanDisbursementInitiated(loan.getLoanAccountMpesa());
-//                }
-//
-//                try {
-//                    B2CRequestResponse b2CRequestResponse = lendingService.disburseLoan(loan);
-//                }catch (Exception exception){
-//                    exception.printStackTrace();
-//                }
-//
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//
-//        });
-//    }
+    @Scheduled(fixedRate = 1000) //every second
+    public void disburseLoans(){
+        List<Loan> pendingDisbursments = lendingService.getAllPendingDisbursements();
+        pendingDisbursments.forEach(loan->{
+
+            try {
+
+                if(investmentManagementService.investorDisburse(loan)){
+                    lendingService.setLoanDisbursementInitiated(loan.getLoanAccountMpesa());
+                }
+
+                try {
+                    B2CRequestResponse b2CRequestResponse = lendingService.disburseLoan(loan);
+                }catch (Exception exception){
+                    exception.printStackTrace();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        });
+    }
 
     @Scheduled(fixedRate = 60 * 60 * 1000) //every 1 hour
     public void checkFailedInvestorRepaymentsEntry(){
